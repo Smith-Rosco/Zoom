28b629acb2d03d839c0925b55f79ce70c82cfd7c
White Milo <inbox.of.milo@gmail.com>
2024-04-14 12:01:55 +0800

    first commit

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,90 @@
+import { cn } from '@/lib/utils'
+import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import EndCallButton from './EndCallButton'
+
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
+
+const MeetingRoom = () => {
+  const [layout, setLayout] = useState('speaker-left')
+  const [showParticipants, setShowParticipants] = useState(false)
+  const isPersonalRoom = useSearchParams().get('personal') === 'true'
+//   const isPersonalRoom = !!useSearchParams().get('personal')
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  const router = useRouter();
+
+
+  if(callingState !== CallingState.JOINED) return
+
+  const CallLayout = () => {
+    switch (layout) {
+        case 'grid':
+            return <PaginatedGridLayout />
+        case 'speaker-left':
+            return <SpeakerLayout participantsBarPosition={"left"} />
+        default:
+            return <SpeakerLayout participantsBarPosition={"right"} />
+    }
+  }
+
+  return (
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+        <div className='relative flex size-full items-center justify-center'>
+            <div className='felx size-full max-w-[1000px] items-center'>
+                <CallLayout />
+            </div>
+            <div className={cn('h-[calc(100vh-86px)] hidden ml-2', { 'show-block': showParticipants})}>
+                <CallParticipantsList onClose={() => setShowParticipants(false)} />
+            </div>
+        </div>
+
+        <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
+            <CallControls onLeave={() => router.push('/')} />
+
+            <DropdownMenu>
+                <div className='flex items-center'>
+                    <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232D] px-4 py-2 hover:bg-[#4C535B]'>
+                        <LayoutList size={20} className='text-white' />
+                    </DropdownMenuTrigger>
+                </div>
+
+                <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+                    {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+                        <div key = {index}>
+                            <DropdownMenuItem className='cursor-pointer'
+                                onClick={() => {
+                                    setLayout(item.toLowerCase() as CallLayoutType)
+                                }}>
+                                {item}
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator className='border-dark-1' />
+                        </div>
+                    ))}
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <CallStatsButton />
+            <button onClick={() => setShowParticipants((prev) => !prev)}>
+                  <div className='cursor-pointer rounded-2xl bg-[#19232D] px-4 py-2 hover:bg-[#4C535B]'>
+                  <Users size={20} className='text-white' />
+                  </div>
+            </button>
+            {!isPersonalRoom && <EndCallButton />}
+        </div>
+    </section>
+  )
+}
+
+export default MeetingRoom
